285c05376993c17963f728240416a4ab9fcdbeb7
GrandLich <seregastepanishev@yandex.ru>
2021-10-25 20:19:25 +0300

    Инициализация проекта

---
+++ src/main/java/dev/mrlich/dogeshop/DogeshopApplication.java
@@ -0,0 +1,13 @@
+package dev.mrlich.dogeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogeshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mrlich/dogeshop/DogeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mrlich.dogeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
